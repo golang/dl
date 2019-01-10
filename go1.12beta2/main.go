7de85f14b44892f6c2cd0a3d141238d1666ff90d
Dmitri Shuralyov <dmitshur@golang.org>
2019-01-10 16:20:01 -0500

    dl: add go1.12beta2
    The Go 1.12 Beta 2 release has been cut, so add a command for it.
    
    Change-Id: Ice22c17efa350e01240ed8808c9ae96ea0ced5f5
    Reviewed-on: https://go-review.googlesource.com/c/157358
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

---
+++ go1.12beta2/main.go
@@ -0,0 +1,24 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.12beta2 command runs the go command from Go 1.12beta2.
+//
+// To install, run:
+//
+//     $ go get golang.org/dl/go1.12beta2
+//     $ go1.12beta2 download
+//
+// And then use the go1.12beta2 command as if it were your normal go
+// command.
+//
+// See the release notes at https://tip.golang.org/doc/go1.12
+//
+// File bugs at https://golang.org/issues/new
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.12beta2")
+}
