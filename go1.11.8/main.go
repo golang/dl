b337094d5ff30366e5fc0651b3127fad366c4abb
Andrew Bonventre <andybons@golang.org>
2019-04-08 17:59:41 -0400

    dl: add go1.11.8 and go1.12.3
    Change-Id: Ifa88285a1ac41ff850245047f801b4f2dec92d89
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/171030
    Reviewed-by: Andrew Bonventre <andybons@golang.org>

---
+++ go1.11.8/main.go
@@ -0,0 +1,24 @@
+// Copyright 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.11.8 command runs the go command from Go 1.11.8.
+//
+// To install, run:
+//
+//     $ go get golang.org/dl/go1.11.8
+//     $ go1.11.8 download
+//
+// And then use the go1.11.8 command as if it were your normal go
+// command.
+//
+// See the release notes at https://golang.org/doc/devel/release.html#go1.11.minor
+//
+// File bugs at https://golang.org/issues/new
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.11.8")
+}
