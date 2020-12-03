c6ce50f27f1501a03a298605655fc7d3f5759d33
Carlos Amedee <carlos@golang.org>
2020-12-03 13:26:35 -0500

    dl: add go1.15.6 and go1.14.13
    For golang/go#42973
    For golang/go#42974
    
    Change-Id: I3ea88b93f39223563f13f8290c8b0244ed4135e7
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/275292
    Run-TryBot: Carlos Amedee <carlos@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Trust: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>

---
+++ go1.15.6/main.go
@@ -0,0 +1,24 @@
+// Copyright 2020 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.15.6 command runs the go command from Go 1.15.6.
+//
+// To install, run:
+//
+//     $ go get golang.org/dl/go1.15.6
+//     $ go1.15.6 download
+//
+// And then use the go1.15.6 command as if it were your normal go
+// command.
+//
+// See the release notes at https://golang.org/doc/devel/release.html#go1.15.minor
+//
+// File bugs at https://golang.org/issues/new
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.15.6")
+}
