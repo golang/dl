4ab242f9ec73af0b5e0dcb8a17eed9a621b0551f
Gopher Robot <gobot@golang.org>
2021-12-09 14:56:39 +0000

    dl: add go1.16.12 and go1.17.5
    Change-Id: Iaed6e38e7868ae2cf997ba0a101f0c3153815c5f
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/370634
    Trust: Alex Rakoczy <alex@golang.org>
    Run-TryBot: Alex Rakoczy <alex@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Filippo Valsorda <filippo@golang.org>

---
+++ go1.16.12/main.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.16.12 command runs the go command from Go 1.16.12.
+//
+// To install, run:
+//
+//     $ go install golang.org/dl/go1.16.12@latest
+//     $ go1.16.12 download
+//
+// And then use the go1.16.12 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.16.minor.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.16.12")
+}
