65ed43b8dfff3387ed1447574622e887227abc00
Gopher Robot <gobot@golang.org>
2021-12-14 19:36:16 +0000

    dl: add go1.18beta1
    Change-Id: Iab92b993410cbb6f11a78d52d0224ac176d0b315
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/371834
    Trust: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Trust: Heschi Kreinick <heschi@google.com>

---
+++ go1.18beta1/main.go
@@ -0,0 +1,24 @@
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.18beta1 command runs the go command from Go 1.18beta1.
+//
+// To install, run:
+//
+//     $ go install golang.org/dl/go1.18beta1@latest
+//     $ go1.18beta1 download
+//
+// And then use the go1.18beta1 command as if it were your normal go
+// command.
+//
+// See the release notes at https://tip.golang.org/doc/go1.18.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.18beta1")
+}
