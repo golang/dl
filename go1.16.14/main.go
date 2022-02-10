08013cf74d42b21dee4732fe39a4fb1bd7caf498
Gopher Robot <gobot@golang.org>
2022-02-10 23:00:19 +0000

    dl: add go1.17.7 and go1.16.14
    Change-Id: I066dbc7089ef3b85e39b4daf4a37289ab6abe10d
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/384954
    Trust: Cherry Mui <cherryyz@google.com>
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    Trust: Dmitri Shuralyov <dmitshur@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>

---
+++ go1.16.14/main.go
@@ -0,0 +1,24 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.16.14 command runs the go command from Go 1.16.14.
+//
+// To install, run:
+//
+//     $ go install golang.org/dl/go1.16.14@latest
+//     $ go1.16.14 download
+//
+// And then use the go1.16.14 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.16.minor.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.16.14")
+}
