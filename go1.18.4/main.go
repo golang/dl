ca0cf14d3e8978e7b42d6f7d350046eff8f2824e
Gopher Robot <gobot@golang.org>
2022-07-12 15:22:49 +0000

    dl: add go1.18.4
    Change-Id: If20395d4c28161478453586c75e8f2ce7584c4f1
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/417134
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    Run-TryBot: Michael Knyszek <mknyszek@google.com>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Auto-Submit: Michael Knyszek <mknyszek@google.com>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Run-TryBot: Gopher Robot <gobot@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ go1.18.4/main.go
@@ -0,0 +1,24 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.18.4 command runs the go command from Go 1.18.4.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.18.4@latest
+//	$ go1.18.4 download
+//
+// And then use the go1.18.4 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.18.minor.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.18.4")
+}
