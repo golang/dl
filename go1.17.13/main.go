5c25facc6e73683a96df02232c453c97da3cac27
Gopher Robot <gobot@golang.org>
2022-07-29 16:11:01 +0000

    dl: add go1.17.13
    Change-Id: I75fd6e0dda222d5784f22aa572ceedc9bb880c84
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/420138
    Run-TryBot: Gopher Robot <gobot@golang.org>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ go1.17.13/main.go
@@ -0,0 +1,24 @@
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.17.13 command runs the go command from Go 1.17.13.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.17.13@latest
+//	$ go1.17.13 download
+//
+// And then use the go1.17.13 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.17.minor.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.17.13")
+}
