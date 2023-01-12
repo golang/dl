626922ede4763a17b6e72cb5c6cdbd184be96f1e
Gopher Robot <gobot@golang.org>
2023-01-12 16:47:05 +0000

    dl: add go1.20rc3
    Change-Id: I353a654b2bf78677a7bb6989a75f4281a76be545
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/461835
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Run-TryBot: Gopher Robot <gobot@golang.org>
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ go1.20rc3/main.go
@@ -0,0 +1,24 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.20rc3 command runs the go command from Go 1.20rc3.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.20rc3@latest
+//	$ go1.20rc3 download
+//
+// And then use the go1.20rc3 command as if it were your normal go
+// command.
+//
+// See the release notes at https://tip.golang.org/doc/go1.20.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.20rc3")
+}
