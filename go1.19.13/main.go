cd520cc22eb4e07de471770390762fc7cf2cdc11
Gopher Robot <gobot@golang.org>
2023-09-06 14:27:21 +0000

    dl: add go1.19.13
    Change-Id: I0ae3509156af00dcf42d6885bbcda2a7d25fcc6c
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/526137
    Run-TryBot: Cherry Mui <cherryyz@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Commit-Queue: Gopher Robot <gobot@golang.org>
    Reviewed-by: Cherry Mui <cherryyz@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Gopher Robot <gobot@golang.org>

---
+++ go1.19.13/main.go
@@ -0,0 +1,24 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.19.13 command runs the go command from Go 1.19.13.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.19.13@latest
+//	$ go1.19.13 download
+//
+// And then use the go1.19.13 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.19.13.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.19.13")
+}
