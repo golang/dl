bd52d20c0808384d8a9a584ed129e0411bfcc50b
Gopher Robot <gobot@golang.org>
2023-10-05 18:32:24 +0000

    dl: add go1.20.9
    Change-Id: Ibb48437efe3a9ed73c72212222ea989d79c0b8b2
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/533235
    Commit-Queue: Gopher Robot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Run-TryBot: Michael Pratt <mpratt@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Reviewed-by: Michael Pratt <mpratt@google.com>

---
+++ go1.20.9/main.go
@@ -0,0 +1,24 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.20.9 command runs the go command from Go 1.20.9.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.20.9@latest
+//	$ go1.20.9 download
+//
+// And then use the go1.20.9 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.20.9.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.20.9")
+}
