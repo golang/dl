8125cd0cb02bf1ec91d6a06e70b95803598e76be
Dmitri Shuralyov <dmitshur@golang.org>
2023-11-07 12:35:25 -0500

    internal/version: add wasip1 support
    Mirror what CL 479621 applied to cmd/go/internal/base/signal_unix.go.
    
    For golang/go#58141.
    Updates golang/go#36976.
    
    Change-Id: I1c625e7d47433e69c00669a3c27dfa34ad45954a
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/540220
    Reviewed-by: Heschi Kreinick <heschi@google.com>
    Auto-Submit: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- internal/version/signal_unix.go
+++ internal/version/signal_unix.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || darwin || dragonfly || freebsd || js || linux || netbsd || openbsd || solaris
-// +build aix darwin dragonfly freebsd js linux netbsd openbsd solaris
+//go:build aix || darwin || dragonfly || freebsd || js || linux || netbsd || openbsd || solaris || wasip1
+// +build aix darwin dragonfly freebsd js linux netbsd openbsd solaris wasip1
 
 package version
 
