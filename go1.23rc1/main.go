20a4bcbb3ee2b496202f5045d2730124c32e2420
Gopher Robot <gobot@golang.org>
2024-06-21 15:37:11 +0000

    dl: add go1.23rc1
    Change-Id: Id4d6989eb6b758531ee1dda79c364d94680445ec
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/593896
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: David Chase <drchase@google.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Reviewed-by: David Chase <drchase@google.com>
    Auto-Submit: Gopher Robot <gobot@golang.org>

---
+++ go1.23rc1/main.go
@@ -0,0 +1,24 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.23rc1 command runs the go command from Go 1.23rc1.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.23rc1@latest
+//	$ go1.23rc1 download
+//
+// And then use the go1.23rc1 command as if it were your normal go
+// command.
+//
+// See the release notes at https://tip.golang.org/doc/go1.23.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.23rc1")
+}
