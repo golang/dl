7654c6f012f97b71a34c7d34e2b680ef2db1e0f7
Gopher Robot <gobot@golang.org>
2024-10-01 16:52:47 +0000

    dl: add go1.23.2
    Change-Id: I745935601dc47334e3e8bcc432327607cfc2b595
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/617017
    Reviewed-by: David Chase <drchase@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Reviewed-by: Michael Knyszek <mknyszek@google.com>

---
+++ go1.23.2/main.go
@@ -0,0 +1,24 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.23.2 command runs the go command from Go 1.23.2.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.23.2@latest
+//	$ go1.23.2 download
+//
+// And then use the go1.23.2 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.23.2.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.23.2")
+}
