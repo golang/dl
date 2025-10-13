810a6b2d12ea1b0ebae8a01c0bc592f4625f7415
Gopher Robot <gobot@golang.org>
2025-10-13 14:00:43 -0700

    dl: add go1.25.3
    Change-Id: I6dc4268b088b845bfcf2d9c7a9019697f4fce56a
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/711460
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ go1.25.3/main.go
@@ -0,0 +1,24 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.25.3 command runs the go command from Go 1.25.3.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.25.3@latest
+//	$ go1.25.3 download
+//
+// And then use the go1.25.3 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.25.3.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.25.3")
+}
