22e1a22285f8fa0226a2c8a38ebb90f14d6da824
Gopher Robot <gobot@golang.org>
2025-12-02 07:55:10 -0800

    dl: add go1.24.11
    Change-Id: I5bd1ccc47caeb44bf886106f41701403f5e91347
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/725860
    Reviewed-by: Mark Freeman <markfreeman@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>

---
+++ go1.24.11/main.go
@@ -0,0 +1,24 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.24.11 command runs the go command from Go 1.24.11.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.24.11@latest
+//	$ go1.24.11 download
+//
+// And then use the go1.24.11 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.24.11.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.24.11")
+}
