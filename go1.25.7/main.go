72e8de5a5ad2ce5dc53ea4f0407f6835eef68e6c
Gopher Robot <gobot@golang.org>
2026-02-04 07:37:39 -0800

    dl: add go1.25.7
    Change-Id: I379c1b3013021b9f060d72355b991ebad9f5fb42
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/741960
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ go1.25.7/main.go
@@ -0,0 +1,24 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.25.7 command runs the go command from Go 1.25.7.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.25.7@latest
+//	$ go1.25.7 download
+//
+// And then use the go1.25.7 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/devel/release#go1.25.7.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.25.7")
+}
