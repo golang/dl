6a105f6841821774d1386cf1a1a14c08f9cc86d2
Gopher Robot <gobot@golang.org>
2026-02-10 11:21:18 -0800

    dl: add go1.26.0
    Change-Id: I6727036f02a0d40c7ad52df232ce01e2a28a1602
    Reviewed-on: https://go-review.googlesource.com/c/dl/+/743960
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Reviewed-by: Junyang Shao <shaojunyang@google.com>
    Auto-Submit: Gopher Robot <gobot@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ go1.26.0/main.go
@@ -0,0 +1,24 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The go1.26.0 command runs the go command from Go 1.26.0.
+//
+// To install, run:
+//
+//	$ go install golang.org/dl/go1.26.0@latest
+//	$ go1.26.0 download
+//
+// And then use the go1.26.0 command as if it were your normal go
+// command.
+//
+// See the release notes at https://go.dev/doc/go1.26.
+//
+// File bugs at https://go.dev/issue/new.
+package main
+
+import "golang.org/dl/internal/version"
+
+func main() {
+	version.Run("go1.26.0")
+}
